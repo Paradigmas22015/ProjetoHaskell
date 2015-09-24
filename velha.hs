ad1771f539b1bdac38cc2a6655cbc9adcc0583d4
BeatrizFerreira <beatriz.216@hotmail.com>
2015-09-24 20:41:54 -0300

    initial logic implementation

--- velha.hs
+++ velha.hs
@@ -15,28 +15,50 @@ iniciarJogo :: String -> String -> IO()
 iniciarJogo jogador1 jogador2 = do
 					putStrLn ("\nPrepare-se para ver a batalha \""
 						++ jogador1 ++ "é X" ++ " vs " ++ jogador2 ++ " é 0" ++ "\"... \n AEWWWW")
-					rodarJogo ['1','2','3','4','5','6','7','8','9']
+					rodarJogo ['1','2','3','4','5','6','7','8','9'] jogador1 jogador2 0
 
-rodarJogo :: Tabela -> IO()
-rodarJogo tabela =  do
-							--{
-							printarTabela tabela
-							--Tem que saber se é a vez do jogador 1 ou jogador 2
-							--Tem pedir o lugar onde inserir X ou 0
-							--colocaXOuYNaTabela é chamada
-							--função dar loop até função verificaTabela retornar true ou se tiver dado velha
-							--}
-							printarTabela tabela
-							putStrLn "O jogo foi encerrado "
+rodarJogo :: Tabela -> Nome -> Nome -> Vez-> IO()
+rodarJogo tabela jogador1 jogador2 vez =  do
+					--{
+					printarTabela tabela
+					--Tem que saber se o jogador 1 venceu
+					--Tem que saber se o jogador 2 venceu
+					--Tem que saber se deu empate
+					--Tem que saber se é a vez do jogador 1 ou jogador 2
+					--Tem pedir o lugar onde inserir X ou 0
+					if (vez == 0) then do
+						putStrLn (jogador1 ++ "eh a sua vez. Escolha uma posicao disponivel!")
+						opcao <- getLine
+						getLine -- descarta Enter
+						rodarJogo (colocaXOuYNaTabela ['X'] opcao tabela) jogador1 jogador2 1
+					else do
+						putStrLn (jogador2 ++ "eh a sua vez. Escolha uma posicao disponivel!")
+						opcao <- getLine
+						getLine -- descarta Enter
+						rodarJogo (colocaXOuYNaTabela ['X'] opcao tabela) jogador1 jogador2 0
+
+					--colocaXOuYNaTabela é chamada
+					--função dar loop até função verificaTabela retornar true ou se tiver dado velha
+					--}
+					printarTabela tabela
+					putStrLn "O jogo foi encerrado "
 
 printarTabela :: Tabela -> IO()
-printarTabela tabela = putStrLn ("Essa função deverar ter um loop que acada 3 loops da uma quebra de linha")
+printarTabela tabela = do
+					putStrLn ((show(tabela!!0)) ++ " | " ++ (show(tabela!!1)) ++ " | " ++ (show(tabela!!2)))
+					putStrLn ((show(tabela!!3)) ++ " | " ++ (show(tabela!!4)) ++ " | " ++ (show(tabela!!5)))
+					putStrLn ((show(tabela!!6)) ++ " | " ++ (show(tabela!!7)) ++ " | " ++ (show(tabela!!8)))
+					putStrLn ("\n")
+
 
 verificaTabela :: Tabela -> IO()
 verificaTabela tabela = do
-											putStrLn ("Essa função deverar verificar se alguem ganhou ou se deu velha")
-											putStrLn ("Alem de printar quem ganhou ou se deu velha")
-											--return True
+					putStrLn ("Essa função deverar verificar se alguem ganhou ou se deu velha")
+					putStrLn ("Alem de printar quem ganhou ou se deu velha")
+					--return True
 
---colocaXOuYNaTabela :: Char -> Char -> Tabela -> Tabela
---colocaXOuYNaTabela identificador posicao tabela =  	putStrLn ("Essa função deverar colocar o identificado na posicao da tabela")
+colocaXOuYNaTabela :: [Char] -> [Char] -> Tabela -> Tabela
+colocaXOuYNaTabela identificador posicao (h:t)
+		| (h == posicao) = (identificador:t)
+		| otherwise = h:(colocaXOuYNaTabela identificador posicao t)
+		--putStrLn ("Essa função deverar colocar o identificado na posicao da tabela")
